10b46f6767af47e8423df0cc0602e9307764fbb0
Andy-Nkumane <nkumaneandy@gmail.com>
2023-03-15 01:27:59 +0200

    added flash message for missing link + changed download buttons from primary to submit types

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, url_for, flash
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 # run this first:
-# export FLASK_APP=<script name>
+# export FLASK_APP=app
 # export FLASK_DEBUG=True
+
+def get_youtube_link():
+    if request.method == 'POST':
+        youtube_link = request.form['youtube-link']
+        if not youtube_link:
+            flash("YouTube link is required!")
+        return youtube_link
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key' # remove from script, get from environmental variable
@@ -16,10 +23,12 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/audio')
+@app.route('/audio', methods=('GET', 'POST'))
 def audio():
+    youtube_link = get_youtube_link()
     return render_template('audio.html')
 
-@app.route('/video')
+@app.route('/video', methods=('GET', 'POST'))
 def video():
+    youtube_link = get_youtube_link()
     return render_template('video.html')
