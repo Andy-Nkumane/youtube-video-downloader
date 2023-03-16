bd55d621790699330a9b8b9d77b2e4da51be706d
Andy-Nkumane <nkumaneandy@gmail.com>
2023-03-16 21:25:17 +0200

    added link validation when clicking download

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+from urllib.parse import urlparse
 
 # run this first:
 # export FLASK_APP=app
@@ -8,9 +9,16 @@ from werkzeug.exceptions import abort
 def get_youtube_link():
     if request.method == 'POST':
         youtube_link = request.form['youtube-link']
-        if not youtube_link:
-            flash("YouTube link is required!")
+        # if not youtube_link:
+        #     flash("YouTube link is required!")
         return youtube_link
+    
+def validate_link(url):
+    parse_result = urlparse(url)
+    is_url = all([parse_result.scheme, parse_result.netloc, parse_result.path])
+    if not is_url:
+        flash('Invalid link entered - enter correct link!')
+    return is_url
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key' # remove from script, get from environmental variable
@@ -26,9 +34,19 @@ def about():
 @app.route('/audio', methods=('GET', 'POST'))
 def audio():
     youtube_link = get_youtube_link()
+    if request.method == 'POST':
+        if validate_link(youtube_link):
+            download_log_existing_data = request.form.get('download-log') or '----\n'
+            download_log_new_data = download_log_existing_data + youtube_link + '-> added\n----\n'
+            return render_template('audio.html', download_log = download_log_new_data)
     return render_template('audio.html')
 
 @app.route('/video', methods=('GET', 'POST'))
 def video():
     youtube_link = get_youtube_link()
+    if request.method == 'POST':
+        if validate_link(youtube_link):
+            download_log_existing_data = request.form.get('download-log') or '----\n'
+            download_log_new_data = download_log_existing_data + youtube_link + '-> added\n----\n'
+            return render_template('video.html', download_log = download_log_new_data )
     return render_template('video.html')
